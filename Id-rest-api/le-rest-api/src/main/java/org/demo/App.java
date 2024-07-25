8b8a5a1fad4e75d8845b3a728cb26df3bc6f0bd0
Dakshnakumar <dakshnakumar018@gmail.com>
2024-07-25 13:07:47 +0530

    sample rest-api executed

--- Id-rest-api/le-rest-api/src/main/java/org/demo/App.java
+++ Id-rest-api/le-rest-api/src/main/java/org/demo/App.java
@@ -1,13 +1,15 @@
 package org.demo;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Java App.........." );
+        System.out.println( "Loading spring & springBoot framework" );
+        SpringApplication.run(App.class,args);
     }
 }
--- Id-rest-api/le-rest-api/src/main/java/org/demo/controller/ProductController.java
+++ Id-rest-api/le-rest-api/src/main/java/org/demo/controller/ProductController.java
@@ -1,10 +1,13 @@
 package org.demo.controller;
 
 import org.demo.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class ProductController {
 
     private List<Product> productList= new ArrayList<>();
@@ -14,7 +17,9 @@ public class ProductController {
      productList.add(new Product("1","mobile",4000,2));
     }
 
+    @GetMapping("/products")
     public List<Product> getAllProducts(){
         return productList;
     }
+
 }
